caca199071616d82fd0fc58a4cb60726d591b54b
Jessica Kiroff <jesskiroff@gmail.com>
2026-03-25 21:18:00 -0400

    add filter method

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var numbers = [3, 56, 2, 48, 5];
 
-function double(x){
-    return x *2;
-}
+// function double(x){
+//     return x *2;
+// }
 
 
-const newNumbers = numbers.map(double);
+// const newNumbers = numbers.map(double);
+// console.log(newNumbers)
+
+const newNumbers = numbers.filter(function(num){
+    return num <10
+});
+
 console.log(newNumbers)
