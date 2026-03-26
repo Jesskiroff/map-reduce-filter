e0de83978bdb702ecffb097847636fc53ecfb02a
Jessica Kiroff <jesskiroff@gmail.com>
2026-03-26 10:46:43 -0400

    add reduce function

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ var numbers = [3, 56, 2, 48, 5];
 // const newNumbers = numbers.map(double);
 // console.log(newNumbers)
 
-const newNumbers = numbers.filter(function(num){
-    return num <10
-});
+// const newNumbers = numbers.filter(function(num){
+//     return num <10
+// });
 
-console.log(newNumbers)
+var newNumber = numbers.reduce(function(accumulator, currentNumber){
+    console.log("accumulator = " + accumulator);
+    console.log("currentNumber = " + currentNumber);
+        return accumulator + currentNumber;
+})
+
+console.log(newNumber)
